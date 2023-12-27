6d21ddc0c26549b898cc85bc035ff7b2477de313
Usmanahmedintern <usman_ahmed_intern@netixsol.com>
2023-12-27 22:25:22 +0500

    Do some changes

--- server/Routers/user.route.js
+++ server/Routers/user.route.js
@@ -1,29 +1,12 @@
-// Import the modules
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../Middleware/userToken');
-const {
-    registerUser,
-    loginUser,
-    signinUser,
-    activateUser,
-    UpdateUser
-} = require('../Controllers/user.controller');
+const userController = require('../Controllers/user.controller'); // Updated import
 
+router.post("/register", userController.registerUser);
+router.post("/activate-user", userController.activateUser);
+router.post("/login", userController.loginUser);
+router.post("/sign-in", verifyToken, userController.signinUser);
+router.post("/update/:id", userController.UpdateUser); // Corrected route parameter syntax
 
-// Route 01 : Method => POST --> Register the User --> localhost:8000/api/auth/register 
-router.post("/register", registerUser);
-// Route 02 : Method => POST --> Register the User --> localhost:8000/api/auth/register 
-router.post("/activate-user", activateUser);
-
-// Route 03 : Method => POST --> login the User --> localhost:8000/api/auth/login`   
-router.post("/login", loginUser)
-
-// Route 04 : Method => POST --> Signin the User --> localhost:8000/api/auth/sign-in`   
-router.post("/sign-in", verifyToken, signinUser)
-
-// Route 04 : Method => POST --> Signin the User --> localhost:8000/api/auth/sign-in`   
-router.post("/update:id", UpdateUser)
-
-// Export the module
 module.exports = router;
