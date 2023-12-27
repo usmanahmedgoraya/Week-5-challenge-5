af7320d5315730199668499f3c28046faa498557
Usmanahmedintern <usman_ahmed_intern@netixsol.com>
2023-12-27 22:28:33 +0500

    ChatGPT Changes App.js

--- server/app.js
+++ server/app.js
@@ -1,44 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-require('./db/connectdb')
-// const ProductsRoute = require('./Routers/Products.route')
-const UserRoute = require('./Routers/user.route')
-const NotesRoute = require("./Routers/Notes.route")
-const CategoryRoute = require("./Routers/Category.route")
+require('./db/connectdb');
 
+const UserRoute = require('./Routers/user.route');
+const NotesRoute = require('./Routers/Notes.route');
+const CategoryRoute = require('./Routers/Category.route');
 
-// * Connecting To Port
 const port = 3000;
 
-// * Automatically parse incoming JSON to an object so we access it in our request handlers
+// Automatically parse incoming JSON to an object so we can access it in our request handlers
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
+// Enable CORS for all routes
 app.use(cors({
-  origin: ["http://localhost:5173","https://week-5-challenge-5-front-end.vercel.app"]
+  origin: ["http://localhost:5173", "https://week-5-challenge-5-front-end.vercel.app"],
+  methods: "GET, POST, OPTIONS, PATCH, DELETE, PUT",
+  credentials: true,
 }));
 
-
-// Set CORS headers manually
-// Add middleware to set CORS headers
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET', 'POST', 'OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  next();
+// Route to check if the server is running
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "success" });
 });
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "success" })
-})
-// app.use("/api", ProductsRoute);
 // User Auth Route
 app.use("/api/auth", UserRoute);
-app.use("/api/notes", NotesRoute)
-app.use("/api/category", CategoryRoute)
+app.use("/api/notes", NotesRoute);
+app.use("/api/category", CategoryRoute);
 
-// * listening To Port
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
+
+// Listening to Port
 app.listen(port, () => {
-  console.log(`This is the ${port} active port! Wait for DB Connection...`);
+  console.log(`Server is running on port ${port}. Waiting for DB Connection...`);
 });
