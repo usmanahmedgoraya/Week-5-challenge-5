50a340ab6ac6fe430a5ea3b99f7fecdc8d036a0c
Usmanahmedintern <usman_ahmed_intern@netixsol.com>
2023-12-31 16:48:27 +0500

    Some Change

--- client/src/components/Form/Login.jsx
+++ client/src/components/Form/Login.jsx
@@ -129,7 +129,7 @@ const Login = () => {
               <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
                 <div className="relative ">
                   <input autoComplete="off" id="email" name="email" type="text" className="peer pt-8 pb-3 placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 dark:text-white focus:outline-none focus:borer-rose-600 dark:bg-cyan-950" placeholder="Email address" onChange={handleInputChange} />
-                  <label htmlFor="email" className="absolute left-0 -top-9 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 dark:peer-placeholder-shown:text-white peer-placeholder-shown:top-1 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 dark:peer-focus:text-white peer-focus:text-sm">Email Address</label>
+                  <label htmlFor="email" className="absolute left-0 -top-5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 dark:peer-placeholder-shown:text-white peer-placeholder-shown:top-1 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 dark:peer-focus:text-white peer-focus:text-sm">Email Address</label>
                 </div>
                 <div className="relative">
                   <input autoComplete="off" id="password" name="password" type="password" className="peer placeholder-transparent h-10 w-full border-b-2 pt-8 pb-3 border-gray-300 text-gray-900 dark:text-white focus:outline-none focus:borer-rose-600 dark:bg-cyan-950" placeholder="Password" onChange={handleInputChange} />
