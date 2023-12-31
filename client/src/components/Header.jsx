fc4c051ed7ffeac5388ecf51d54ed34ec261ff66
Usmanahmedintern <usman_ahmed_intern@netixsol.com>
2023-12-31 17:25:57 +0500

    Add the icon Home Link

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,12 +19,12 @@ const Header = () => {
         <div>
             <header className="text-gray-400 bg-gray-900 body-font flex items-center">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center justify-between">
-                    <a className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
+                    <Link to={"/"} className="flex title-font font-medium items-center text-white mb-4 md:mb-0">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
                             <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
                         </svg>
                         <Link to={"/"} className="ml-3 text-xl">Upnote</Link>
-                    </a>
+                    </Link>
                     <div className="flex space-x-5 items-center mt-4">
 
                         {
